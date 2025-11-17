d60ab755e9dae9d92c333d52d57db0ca0f5b814d
Max <mc.cui@mail.utoronto.ca>
2025-11-17 15:29:06 -0500

    Add R timeseries project and untrack dataset files

---
+++ RProjects/normalize_dataset.py
@@ -0,0 +1,45 @@
+from zipfile import ZipFile
+import pandas as pd # type: ignore
+import keras # type: ignore
+
+uri = "https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip"
+zip_path = keras.utils.get_file(origin=uri, fname="jena_climate_2009_2016.csv.zip")
+zip_file = ZipFile(zip_path)
+zip_file.extractall()
+csv_path = "jena_climate_2009_2016.csv"
+
+df = pd.read_csv(csv_path)
+
+feature_keys = [
+    "p (mbar)",
+    "T (degC)",
+    "Tpot (K)",
+    "Tdew (degC)",
+    "rh (%)",
+    "VPmax (mbar)",
+    "VPact (mbar)",
+    "VPdef (mbar)",
+    "sh (g/kg)",
+    "H2OC (mmol/mol)",
+    "rho (g/m**3)",
+    "wv (m/s)",
+    "max. wv (m/s)",
+    "wd (deg)",
+]
+
+split_fraction = 0.715
+train_split = int(split_fraction * int(df.shape[0]))
+
+def normalize(data, train_split):
+    data_mean = data[:train_split].mean(axis=0)
+    data_std = data[:train_split].std(axis=0)
+    return (data - data_mean) / data_std
+
+selected_features = [feature_keys[i] for i in [0, 1, 5, 7, 8, 10, 11]]
+features = df[selected_features]
+features.index = df["Date Time"]
+
+features = normalize(features.values, train_split)
+features = pd.DataFrame(features)
+
+df.to_csv("jena_climate_2009_2016.csv", index=False)
